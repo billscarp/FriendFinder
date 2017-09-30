a2698c669ff2521b5bcab2b60715ba6c351ca89a
billscarp <billscarp@gmail.com>
2017-09-30 12:48:57 -0400

    quiz routing/server.js

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+var friends = [
+	{
+		"God": "Jimi Hendrix",
+		"gif": "https://media.giphy.com/media/MM8S7mHc1S7lK/giphy.gif",
+		
+	},
+	{
+		"God": "Buddy Guy",
+		"gif": "https://www.morrisonhotelgallery.com/images/medium/Buddy_Guy_B_900px.jpg",
+		
+	},
+	{
+		"God": "Eddie Van Halen  ",
+		"gif": "https://media.giphy.com/media/Ea1pvZkUIlZHa/giphy.gif",
+		
+	},
+	{
+		"God": "Eric Clapton",
+		"gif": "https://media.giphy.com/media/JRX6UOE32vkZy/giphy.gif",
+		
+	},
+	{
+		"God": "Zakk Wylde",
+		"gif": "https://media.giphy.com/media/itiN11Uzajn68/giphy.gif",
+		
+	},
+];
+
+//export
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
+var apiRoutes = require("./app/routing/htmlRoutes.js");
+var htmlRoutes = require("./app/routing/apiRoutes.js");
+
+pageShow(app, __dirname);
